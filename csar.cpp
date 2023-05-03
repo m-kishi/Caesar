8cb850f3de1ceeb0e6604c9165315852fc799734
m.kishi <m.kishi.5@gmail.com>
2023-05-03 11:33:20 +0900

    UPD:Improved evaluate function

--- csar.cpp
+++ csar.cpp
@@ -17,8 +17,10 @@
 // --------------------------------------------------
 // アルファ・ベータ法による探索
 int alpha_beta_pruning(board_t*, player_t, int, int, int, bitboard_t[]);
-// 評価値を計算する
-int evaluation_value(board_t*);
+// 評価関数
+int evaluate(board_t*);
+// 盤面の重み付け方
+int cell_weight(bitboard_t);
 // 盤面の状態をfromからdestへコピーする
 void board_copy(board_t*, board_t*);
 
@@ -41,7 +43,7 @@ int alpha_beta_pruning(board_t *board, player_t player, int depth, int alpha, in
   // 想定の深さまで到達するか終局したら探索終了
   if (depth == 0 || board->status == OVER) {
     // 現在の局面の評価値を返却する
-    return evaluation_value(board);
+    return evaluate(board);
   }
 
   // 盤面のバックアップ
@@ -101,17 +103,131 @@ int alpha_beta_pruning(board_t *board, player_t player, int depth, int alpha, in
 }
 
 // ==================================================
-// 評価値を計算する
+// 評価関数
 // 単純に"自石の数-他石の数"を評価値とする
 // ==================================================
-int evaluation_value(board_t *board) {
-  int nblack = count_of_discs(board->black);
-  int nwhite = count_of_discs(board->white);
-  if (board->player == BLACK) {
-    return nblack - nwhite;
-  } else {
-    return nwhite - nblack;
+//int evaluate(board_t *board) {
+//  int nblack = count_of_discs(board->black);
+//  int nwhite = count_of_discs(board->white);
+//  if (board->player == BLACK) {
+//    return nblack - nwhite;
+//  } else {
+//    return nwhite - nblack;
+//  }
+//}
+
+// ==================================================
+// 評価関数
+// ==================================================
+int evaluate(board_t *board) {
+  // 黒白それぞれの評価値
+  int black = 0, white = 0;
+  // 左上から調べていく
+  bitboard_t mv;
+  bitboard_t pos = 0x8000000000000000;
+  for (; pos != 0; pos = pos >> 1) {
+    // 黒マスの重みを加算
+    mv = (board->black & pos);
+    if (mv != 0) {
+      black += cell_weight(mv);
+      continue;
+    }
+    // 白マスの重みを加算
+    mv = (board->white & pos);
+    if (mv != 0) {
+      white += cell_weight(mv);
+      continue;
+    }
   }
+  // 現在の手番から見た評価値を返す
+  if (board->player == BLACK) {
+    return black - white;
+  } else {
+    return white - black;
+  }
+}
+
+// ==================================================
+// 盤面の重み付け方
+// |  30 | -12 |  0 | -1 | -1 |  0 | -12 |  30 |
+// | -12 | -15 | -3 | -3 | -3 | -3 | -15 | -12 |
+// |   0 |  -3 |  0 | -1 | -1 |  0 |  -3 |   0 |
+// |  -1 |  -3 | -1 | -1 | -1 | -1 |  -3 |  -1 |
+// |  -1 |  -3 | -1 | -1 | -1 | -1 |  -3 |  -1 |
+// |   0 |  -3 |  0 | -1 | -1 |  0 |  -3 |   0 |
+// | -12 | -15 | -3 | -3 | -3 | -3 | -15 | -12 |
+// |  30 | -12 |  0 | -1 | -1 |  0 | -12 |  30 |
+// ==================================================
+int cell_weight(bitboard_t mv) {
+    switch (mv) {
+      case 0x1000000000000000:
+      case 0x0800000000000000:
+      case 0x0000100000000000:
+      case 0x0000080000000000:
+      case 0x0000008000000000:
+      case 0x0000002000000000:
+      case 0x0000001000000000:
+      case 0x0000000800000000:
+      case 0x0000000400000000:
+      case 0x0000000100000000:
+      case 0x0000000080000000:
+      case 0x0000000020000000:
+      case 0x0000000010000000:
+      case 0x0000000008000000:
+      case 0x0000000004000000:
+      case 0x0000000001000000:
+      case 0x0000000000100000:
+      case 0x0000000000080000:
+      case 0x0000000000000010:
+      case 0x0000000000000008:
+        return -1;
+      case 0x0020000000000000:
+      case 0x0010000000000000:
+      case 0x0008000000000000:
+      case 0x0004000000000000:
+      case 0x0000400000000000:
+      case 0x0000020000000000:
+      case 0x0000004000000000:
+      case 0x0000000200000000:
+      case 0x0000000040000000:
+      case 0x0000000002000000:
+      case 0x0000000000400000:
+      case 0x0000000000020000:
+      case 0x0000000000002000:
+      case 0x0000000000001000:
+      case 0x0000000000000800:
+      case 0x0000000000000400:
+        return -3;
+      case 0x2000000000000000:
+      case 0x0400000000000000:
+      case 0x0000800000000000:
+      case 0x0000200000000000:
+      case 0x0000040000000000:
+      case 0x0000010000000000:
+      case 0x0000000000800000:
+      case 0x0000000000200000:
+      case 0x0000000000040000:
+      case 0x0000000000010000:
+      case 0x0000000000000020:
+      case 0x0000000000000004:
+        return 0;
+      case 0x4000000000000000:
+      case 0x0200000000000000:
+      case 0x0080000000000000:
+      case 0x0001000000000000:
+      case 0x0000000000008000:
+      case 0x0000000000000100:
+      case 0x0000000000000040:
+      case 0x0000000000000002:
+        return -12;
+      case 0x0040000000000000:
+      case 0x0002000000000000:
+      case 0x0000000000004000:
+      case 0x0000000000000200:
+        return -15;
+      default:
+        return 30;
+    }
 }
 
 // ==================================================
